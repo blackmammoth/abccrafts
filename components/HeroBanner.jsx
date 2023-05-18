863ae7e11517cb10b34cefbc0a7bf5217e262155
blackmammoth <118998054+blackmammoth@users.noreply.github.com>
2023-05-18 21:08:56 +0300

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,7 +5,7 @@ import { urlFor } from '../lib/client';
 
 const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className="hero-banner-container">
+    <div className="hero-banner-container" style={{display: none;}}>
       <div className='hero-banner-desc'>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
